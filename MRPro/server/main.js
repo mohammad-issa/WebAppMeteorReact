9f7ff6dff4265b739514da71cf115bfa122e5663
Mohammad issa <asali@mxd3d.com>
2017-09-20 22:23:57 +0300

    meteor init

---
+++ MRPro/server/main.js
@@ -0,0 +1,9 @@
+// import { Meteor } from 'meteor/meteor';
+
+// Meteor.startup(() => {
+//   // code to run on server at startup
+// });
+import addDefault from './../imports/math'
+// import './../imports/math'
+
+console.log(addDefault(8,8));
